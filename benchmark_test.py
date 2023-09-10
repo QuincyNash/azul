e8e7518f321b1364e9cec745e86af207b499a484
quincynash <quincy.nash@icloud.com>
2023-09-10 09:49:20 -0700

    Rename tests; add test_serialize

--- benchmark_test.py
+++ benchmark_test.py
@@ -1,31 +1,37 @@
+from collections import Counter
 from pytest_benchmark.fixture import BenchmarkFixture as Benchmark
-from game import Game
+from game import Game, Move
 from search import negascout
 from evaluation import game_evaluation, load_player_eval
 
 
-def test_moves_benchmark(benchmark: Benchmark):
+def test_serialize(benchmark: Benchmark):
+    game = Game(seed=0)
+    benchmark(game.serialize)
+
+
+def test_moves(benchmark: Benchmark):
     game = Game(seed=0)
     benchmark(game.all_moves, 0)
 
 
-def test_state_benchmark(benchmark: Benchmark):
+def test_state(benchmark: Benchmark):
     game = Game(seed=0)
     benchmark(game.get_state_after_move, 0, game.all_moves(0)[0])
 
 
-def test_points_benchmark(benchmark: Benchmark):
+def test_points(benchmark: Benchmark):
     game = Game(seed=0)
     benchmark(game.calculate_points, flag="include_bonus")
 
 
-def test_negascout_benchmark(benchmark: Benchmark):
+def test_negascout(benchmark: Benchmark):
     game = Game(seed=0)
     player_eval = load_player_eval("v2")
     benchmark(negascout, player_eval, game, 0, 2, 2)
 
 
-def test_evaluation_benchmark(benchmark: Benchmark):
+def test_evaluation(benchmark: Benchmark):
     game = Game(seed=0)
     player_eval = load_player_eval("v2")
     benchmark(game_evaluation, game, player_eval["player_evaluation"])
