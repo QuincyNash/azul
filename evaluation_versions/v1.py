5ed71be609f6b475ac2437124bc31e44c5bad4b5
quincynash <quincy.nash@icloud.com>
2023-09-24 21:47:46 -0700

    Use PointsResult

--- evaluation_versions/v1.py
+++ evaluation_versions/v1.py
@@ -1,13 +1,13 @@
-from game import Game, Move
+from game import Game, Move, PointsResult
 from player import Player
 import random
 
 
 # Evaluates a players position
-def player_evaluation(game: Game, player: Player) -> float:
+def player_evaluation(points_result: PointsResult) -> float:
     return random.random()
 
 
 # Decides how promising a move is
-def move_potential(move: Move) -> float:
+def move_potential(game: Game, move: Move) -> float:
     return move.amount - len(move.floor_tiles)
