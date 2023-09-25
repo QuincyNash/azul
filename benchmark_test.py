e62f5b93a77310c7501f1f81aff1e87b79c76985
quincynash <quincy.nash@icloud.com>
2023-09-24 21:41:28 -0700

    remove test_negascout

--- benchmark_test.py
+++ benchmark_test.py
@@ -1,3 +1,4 @@
+from constants import *
 from collections import Counter
 from pytest_benchmark.fixture import BenchmarkFixture as Benchmark
 from game import Game, Move
@@ -5,9 +6,19 @@ from search import negascout
 from evaluation import game_evaluation, load_player_eval
 
 
+def test_negascout(benchmark: Benchmark):
+    game = Game(seed=0)
+    benchmark(negascout, load_player_eval(EVALUATION_VERSION), game, 0, 2, 2)
+
+
 def test_serialize(benchmark: Benchmark):
     game = Game(seed=0)
     benchmark(game.serialize)
+
+
+def test_no_moves(benchmark: Benchmark):
+    game = Game(seed=0)
+    benchmark(game.are_no_moves)
 
 
 def test_moves(benchmark: Benchmark):
@@ -15,23 +26,24 @@ def test_moves(benchmark: Benchmark):
     benchmark(game.all_moves, 0)
 
 
-def test_state(benchmark: Benchmark):
+def test_make_move(benchmark: Benchmark):
     game = Game(seed=0)
-    benchmark(game.get_state_after_move, 0, game.all_moves(0)[0])
+    benchmark(game.make_move, 0, game.all_moves(0)[0])
+
+
+def test_undo_move(benchmark: Benchmark):
+    game = Game(seed=0)
+    benchmark(game.undo_move, 0, game.all_moves(0)[0])
 
 
 def test_points(benchmark: Benchmark):
     game = Game(seed=0)
-    benchmark(game.calculate_points, flag="include_bonus")
-
-
-def test_negascout(benchmark: Benchmark):
-    game = Game(seed=0)
-    player_eval = load_player_eval("v2")
-    benchmark(negascout, player_eval, game, 0, 2, 2)
+    func = lambda: game.calculate_points(flag="include_bonus")
+    benchmark(func)
 
 
 def test_evaluation(benchmark: Benchmark):
     game = Game(seed=0)
+    points_result = game.calculate_points(flag="include_bonus")
     player_eval = load_player_eval("v2")
-    benchmark(game_evaluation, game, player_eval["player_evaluation"])
+    benchmark(game_evaluation, points_result, player_eval["player_evaluation"])
