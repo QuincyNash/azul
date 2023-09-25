10be3e8d1e379b3a81e44f88b89cf4f240fa5e67
quincynash <quincy.nash@icloud.com>
2023-09-24 21:42:59 -0700

    pass game to player_evaluation

--- evaluation.py
+++ evaluation.py
@@ -1,13 +1,13 @@
 from typing import Callable, TypedDict
 from constants import *
-from game import Game, Move
+from game import PointsResult, Game, Move
 from player import Player
 from importlib import import_module
 
 
 class EvaluationVersion(TypedDict):
-    player_evaluation: Callable[[Game, Player], float]
-    move_potential: Callable[[Move], float]
+    player_evaluation: Callable[[PointsResult], float]
+    move_potential: Callable[[Game, Move], float]
 
 
 def load_player_eval(version: str) -> EvaluationVersion:
@@ -22,17 +22,20 @@ def load_player_eval(version: str) -> EvaluationVersion:
 
 # Evaluation always ranks the position from the point of view of player 1
 def game_evaluation(
-    game: Game, player_evaluation: Callable[[Game, Player], float]
+    points_results: List[PointsResult],
+    player_evaluation: Callable[[PointsResult], float],
 ) -> float:
-    player1 = player_evaluation(game, game.players[0])
-    player2 = player_evaluation(game, game.players[1])
+    player1 = player_evaluation(points_results[0])
+    player2 = player_evaluation(points_results[1])
 
     return player1 - player2
 
 
 # Considers who the active player is
 def game_evaluation_for_player(
-    turn: int, game: Game, player_evaluation: Callable[[Game, Player], float]
+    turn: int,
+    points_results: List[PointsResult],
+    player_evaluation: Callable[[PointsResult], float],
 ) -> float:
     multiplier = 1 if turn == 0 else -1
-    return multiplier * game_evaluation(game, player_evaluation)
+    return multiplier * game_evaluation(points_results, player_evaluation)
