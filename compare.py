a7499e23b7747350bab9d33cfc7a93f301a3ce5d
quincynash <quincy.nash@icloud.com>
2023-09-25 19:43:40 -0700

    Add multiprocessing; remove graphics display

--- compare.py
+++ compare.py
@@ -1,84 +1,106 @@
 from constants import *
-from typing import Union
-from evaluation import EvaluationVersion, load_player_eval
+from typing import TypedDict
+from evaluation import load_player_eval
 from game import Game
-from animation import Animation
-from player import Player
-from search import get_best_move, SearchedNode
+from search import get_best_move
+import multiprocessing
+import random
+import json
 from tqdm import tqdm
-from importlib import import_module
 
 
-# start = time.perf_counter()
-# for i in range(10000):
-#     game.all_moves(0)
-# print(time.perf_counter() - start)
+class Score(TypedDict):
+    player1_wins: int
+    player2_wins: int
+    ties: int
 
-player1_eval = load_player_eval(PLAYER1_COMPARE_VERSION)
-player2_eval = load_player_eval(PLAYER2_COMPARE_VERSION)
 
-game = Game()
+player1_eval = load_player_eval(f"v{PLAYER1_COMPARE_VERSION}")
+player2_eval = load_player_eval(f"v{PLAYER2_COMPARE_VERSION}")
 
-pygame.event.set_allowed([pygame.QUIT])
-pygame.display.set_caption("Azul")
 
-quit = False
-end = False
-turn = 0
-game_round = 1
-player1_wins = 0
-player2_wins = 0
-ties = 0
+# Return of 0 represents draw, 1 represents player 1 win, 2 represents player 2 win
+def play_game(move_time: float) -> int:
+    turn = random.randint(0, 1)
+    game = Game(graphics_info=None)
 
-game.render(player1_wins=0, player2_wins=0, ties=0)
-pygame.display.update()
-
-while not quit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit = True
-
-    if not end:
+    while not game.is_game_over():
         if game.is_round_over():
             game.calculate_points_and_modify()
-
-            # Restart game if finished
-            if game.is_game_over():
-                game.calculate_points_and_modify(flag="bonus_only")
-
-                score_difference = game.players[0].points - game.players[1].points
-                if score_difference > 0:
-                    player1_wins += 1
-                elif score_difference < 0:
-                    player2_wins += 1
-                else:
-                    ties += 1
-
-                if game_round >= NUM_COMPARE_ROUNDS:
-                    end = True
-                else:
-                    game = Game()
-                    turn = 0
-                    game_round += 1
-
-                print(player1_wins, player2_wins, ties)
-
-            # Restart round if finished
-            else:
-                # Assign turn to whoever drew the starting marker
-                turn = game.new_round()
+            turn = game.new_round()
 
         else:
             result = get_best_move(
-                player1_eval, player2_eval, game, turn, COMPARE_COMPUTER_MOVE_TIME
+                player1_eval,
+                player2_eval,
+                game,
+                turn,
+                move_time,
+                show_progress=False,
             )
-
-            # print(result.score, result.nodes_searched)
-
             game.make_move(turn, result.move)
             turn = (turn + 1) % 2
 
-        game.render(player1_wins=player1_wins, player2_wins=player2_wins, ties=ties)
-        pygame.display.update()
+    game.calculate_points_and_modify(flag="bonus_only")
 
-        clock.tick(FRAME_RATE)
+    score_difference = game.players[0].points - game.players[1].points
+    if score_difference > 0:
+        return 1
+    elif score_difference < 0:
+        return 2
+    else:
+        return 0
+
+
+if __name__ == "__main__":
+    player1_wins = 0
+    player2_wins = 0
+    ties = 0
+
+    # Play NUM_COMPARE_ROUNDS games in parallel and record the results
+    with multiprocessing.Pool() as pool:
+        for result in tqdm(
+            pool.imap_unordered(
+                play_game, [COMPARE_COMPUTER_MOVE_TIME] * NUM_COMPARE_ROUNDS
+            ),
+            total=NUM_COMPARE_ROUNDS,
+        ):
+            if result == 0:
+                ties += 1
+            elif result == 1:
+                player1_wins += 1
+            elif result == 2:
+                player2_wins += 1
+
+    file = open("compare.json", "r+", encoding="utf-8")
+
+    # Information stored as a dictionary of different combinations of versions eg. (v1,v2)
+    current_text: Dict[str, Score] = json.load(file)
+
+    smaller_version_number = min(PLAYER1_COMPARE_VERSION, PLAYER2_COMPARE_VERSION)
+    larger_version_number = max(PLAYER1_COMPARE_VERSION, PLAYER2_COMPARE_VERSION)
+    version_combination = f"v{smaller_version_number},v{larger_version_number}"
+
+    # Add the new information to the dictionary
+    if version_combination not in current_text:
+        current_text[version_combination] = {
+            "player1_wins": player1_wins,
+            "player2_wins": player2_wins,
+            "ties": ties,
+        }
+    else:
+        current_text[version_combination]["player1_wins"] += player1_wins
+        current_text[version_combination]["player2_wins"] += player2_wins
+        current_text[version_combination]["ties"] += ties
+
+    # Overwrite the existing file
+    file.seek(0)
+    json.dump(current_text, file, ensure_ascii=False, indent=4)
+    file.truncate()
+
+    print(
+        f"Player 1 Wins: {player1_wins}",
+        f"Player 2 Wins: {player2_wins}",
+        f"Ties: {ties}",
+        sep="\n",
+    )
