414a414bd5affef4c789018d292e75170bc56a79
quincynash <quincy.nash@icloud.com>
2023-11-24 22:09:26 -0800

    Improve progress bars; Fix version numbers

--- compare.py
+++ compare.py
@@ -1,5 +1,5 @@
 from constants import *
-from typing import TypedDict
+from typing import TypedDict, Dict
 from evaluation import load_player_eval
 from game import Game
 from search import get_best_move
@@ -15,8 +15,8 @@ class Score(TypedDict):
     ties: int
 
 
-player1_eval = load_player_eval(f"v{PLAYER1_COMPARE_VERSION}")
-player2_eval = load_player_eval(f"v{PLAYER2_COMPARE_VERSION}")
+player1_eval = load_player_eval(PLAYER1_COMPARE_VERSION)
+player2_eval = load_player_eval(PLAYER2_COMPARE_VERSION)
 
 
 # Return of 0 represents draw, 1 represents player 1 win, 2 represents player 2 win
@@ -59,12 +59,15 @@ if __name__ == "__main__":
 
     # Play NUM_COMPARE_ROUNDS games in parallel and record the results
     with multiprocessing.Pool() as pool:
-        for result in tqdm(
+        progress_bar = tqdm(
             pool.imap_unordered(
                 play_game, [COMPARE_COMPUTER_MOVE_TIME] * NUM_COMPARE_ROUNDS
             ),
             total=NUM_COMPARE_ROUNDS,
-        ):
+            postfix={"Score": f"{player1_wins}-{player2_wins}-{ties}"},
+        )
+
+        for result in progress_bar:
             if result == 0:
                 ties += 1
             elif result == 1:
@@ -72,14 +75,19 @@ if __name__ == "__main__":
             elif result == 2:
                 player2_wins += 1
 
+            progress_bar.set_postfix({"Score": f"{player1_wins}-{player2_wins}-{ties}"})
+
     file = open("compare.json", "r+", encoding="utf-8")
 
     # Information stored as a dictionary of different combinations of versions eg. (v1,v2)
     current_text: Dict[str, Score] = json.load(file)
 
-    smaller_version_number = min(PLAYER1_COMPARE_VERSION, PLAYER2_COMPARE_VERSION)
-    larger_version_number = max(PLAYER1_COMPARE_VERSION, PLAYER2_COMPARE_VERSION)
-    version_combination = f"v{smaller_version_number},v{larger_version_number}"
+    player1_version_number = int(PLAYER1_COMPARE_VERSION[1:])
+    player2_version_number = int(PLAYER2_COMPARE_VERSION[1:])
+
+    smaller_version_number = min(player1_version_number, player2_version_number)
+    larger_version_number = max(player1_version_number, player2_version_number)
+    version_combination = f"v{larger_version_number},v{smaller_version_number}"
 
     # Add the new information to the dictionary
     if version_combination not in current_text:
@@ -95,7 +103,7 @@ if __name__ == "__main__":
 
     # Overwrite the existing file
     file.seek(0)
-    json.dump(current_text, file, ensure_ascii=False, indent=4)
+    json.dump(current_text, file, ensure_ascii=False, indent=2)
     file.truncate()
 
     print(
