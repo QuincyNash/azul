0baa60af62228b1a4cde4b5b118a1b07f70a1c86
quincynash <quincy.nash@icloud.com>
2023-11-24 22:12:58 -0800

    Readability improvements

--- main.py
+++ main.py
@@ -7,97 +7,93 @@ import graphics
 from search import get_best_move
 import time
 import pygame
+import json
 
 
-if __name__ == "__main__":
-    # start = time.perf_counter()
-    # for i in range(10000):
-    #     game.all_moves(0)
-    # print(time.perf_counter() - start)
+# start = time.perf_counter()
+# for i in range(10000):
+#     game.all_moves(0)
+# print(time.perf_counter() - start)
 
-    graphics_info = graphics.init()
-    game = Game(graphics_info=graphics_info, seed=10)
+graphics_info = graphics.init()
+game = Game(graphics_info=graphics_info, seed=10)
+game.from_json(json.load(open("game_state.json", "r")))
 
-    pygame.event.set_allowed([pygame.QUIT])
-    pygame.display.set_caption("Azul")
+pygame.event.set_allowed([pygame.QUIT])
+pygame.display.set_caption("Azul")
 
-    quit = False
-    end = False
-    animation: Union[Animation, None] = None
-    new_game = game.copy()
-    serialized_game = game.serialize()
-    turn = 0
-    player1_wins = 0
-    player2_wins = 0
+quit = False
+end = False
+animation: Union[Animation, None] = None
+new_game = game.copy()
+serialized_game = game.serialize()
+turn = 0
+player1_wins = 0
+player2_wins = 0
 
-    eval_version = load_player_eval(EVALUATION_VERSION)
+eval_version = load_player_eval(EVALUATION_VERSION)
 
-    game.render()
-    pygame.display.update()
+while not quit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit = True
 
-    while not quit:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                quit = True
+    if not end and animation:
+        if animation.finished:
+            animation = None
 
-        if not end and animation:
-            if animation.finished:
-                animation = None
-
-                # If the animation is over and the round is over, start a new round
-                if game.is_round_over():
-                    game = new_game.copy()
-
-                    # If the game is over, calcuolate the final score and end the game
-                    if game.is_game_over():
-                        game.calculate_points_and_modify(flag="bonus_only")
-                        end = True
-                    else:
-                        turn = game.new_round()
-
-                # If the animation is over, set the new game state for the next move
-                else:
-                    game = new_game.copy()
-
-                game.render()
-                pygame.display.update()
-                time.sleep(PAUSE_TIME_AFTER_MOVE)
-
-            # Update and render animation
-            else:
-                game.render(no_tiles_but_wall=True)
-                animation.render()
-                pygame.display.update()
-
-                animation.update()
-
-        elif not end:
-            # Start the wall tiling animation
+            # If the animation is over and the round is over, start a new round
             if game.is_round_over():
-                new_game = game.copy()
-                new_game.calculate_points_and_modify()
+                game = new_game.copy()
 
-                animation = Animation(turn, game, None, new_game, graphics_info)
+                # If the game is over, calcuolate the final score and end the game
+                if game.is_game_over():
+                    game.calculate_points_and_modify(flag="bonus_only")
+                    end = True
+                else:
+                    turn = game.new_round()
 
-                game.render()
-                pygame.display.update()
-
-            # Start the move animation
+            # If the animation is over, set the new game state for the next move
             else:
-                result = get_best_move(
-                    eval_version, eval_version, game, turn, COMPUTER_MOVE_TIME
-                )
+                game = new_game.copy()
 
-                print(result.score, result.nodes_searched, result.transposition_lookups)
+            game.render()
+            pygame.display.update()
+            time.sleep(PAUSE_TIME_AFTER_MOVE)
 
-                game.make_move(turn, result.move)
-                new_game = game.copy()
-                game.undo_move(turn, result.move)
+        # Update and render animation
+        else:
+            game.render(no_tiles_but_wall=True)
+            animation.render()
+            pygame.display.update()
 
-                animation = Animation(turn, game, result.move, new_game, graphics_info)
-                turn = (turn + 1) % 2
+            animation.update()
+
+    elif not end:
+        # Start the wall tiling animation
+        if game.is_round_over():
+            new_game = game.copy()
+            new_game.calculate_points_and_modify()
+
+            animation = Animation(turn, game, None, new_game, graphics_info)
 
             game.render()
             pygame.display.update()
 
-        graphics_info.clock.tick(FRAME_RATE)
+        # Start the move animation
+        else:
+            result = get_best_move(
+                eval_version, eval_version, game, turn, COMPUTER_MOVE_TIME
+            )
+
+            game.make_move(turn, result.move)
+            new_game = game.copy()
+            game.undo_move(turn, result.move)
+
+            animation = Animation(turn, game, result.move, new_game, graphics_info)
+            turn = (turn + 1) % 2
+
+        game.render()
+        pygame.display.update()
+
+    graphics_info.clock.tick(FRAME_RATE)
